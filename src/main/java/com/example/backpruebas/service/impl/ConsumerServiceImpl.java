5973b6ce69a1a3e3c56bae83b12902a3d16a50df
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-10-25 20:28:02 -0500

    Actualización del Consumer

--- src/main/java/com/example/backpruebas/service/impl/ConsumerServiceImpl.java
+++ src/main/java/com/example/backpruebas/service/impl/ConsumerServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.backpruebas.service.ConsumerService;
 import com.example.backpruebas.service.PruebaService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.AmqpIOException;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -17,43 +19,64 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-public class ConsumerServiceImpl{
-    /*@Autowired
+public class ConsumerServiceImpl implements ConsumerService {
+    @Autowired
     private PruebaService pruebaService;
 
     @Override
-    @RabbitListener(
+    /*@RabbitListener(
             bindings = @QueueBinding(
-                    value = @Queue(value = "${spring.rabbitmq.queue}",durable = "true"),
+                    value = @Queue(value = "${spring.rabbitmq.queue}", durable = "true"),
                     exchange = @Exchange(value = "${spring.rabbitmq.exchange}"),
                     key = "${spring.rabbitmq.routingkey}")
+    )*/
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value = "pruebas.rpc.requests", durable = "true"),
+                    exchange = @Exchange(value = "pruebas.rpc"),
+                    key = "${spring.rabbitmq.routingkey}")
     )
+
     public Object consumerMessage(String objId) throws AmqpIOException {
+
         System.out.println("=============== Message ==================");
         System.out.println(objId);
         System.out.println("==========================================");
+
         UUID pruebaId;
-        try{
-            pruebaId= UUID.fromString(objId);
+
+        try {
+            pruebaId = UUID.fromString(objId);
         } catch (Exception e) {
             ObjectMapper obj = new ObjectMapper();
             try {
                 return obj.writeValueAsString("Error: El id de la prueba no es un UUID válido");
-            }catch(JsonProcessingException ex){
+            } catch (JsonProcessingException ex) {
                 return null;
             }
         }
-        Prueba objinst= pruebaService.getPrueba(pruebaId);
-        if(objinst==null){
+
+        Prueba product = pruebaService.getPrueba(pruebaId);
+
+        if (product == null) {
+            System.out.println("No existe una prueba con ese ID");
             return null;
-        }
-        else{
-            ObjectMapper obj = new ObjectMapper();
+        } else {
+
+            System.out.println("Existe la prueba");
+            System.out.println(product.toString());
+
+            ObjectMapper obj = JsonMapper.builder()
+                    .addModule(new JavaTimeModule())
+                    .build();
+
             try {
-                return obj.writeValueAsString(objinst);
-            }catch(JsonProcessingException e){
+                return obj.writeValueAsString(product).replaceAll("^\\{\\Xid\\X:\\d+,", "{"); //test
+            } catch (JsonProcessingException e) {
+                System.out.println(e.toString());
                 return null;
             }
         }
-    }*/
+
+    }
 }
